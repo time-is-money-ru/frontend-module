a38cbcd74f165c46cae52c4eee8d81370582f275
Storozhuk Valentine <stolentine@gmail.com>
2024-12-05 21:15:32 +0300

    из простого примера убрана вставка через JS

--- example/simple/index.js
+++ example/simple/index.js
@@ -5,16 +5,7 @@ window.addEventListener('load', () => {
   });
 
   document.getElementById('timButton').addEventListener('click', () => {
-      timClient.getDocuments(['passport', "INN"])
-        .then(documents => {
-          document.getElementById('firstName').value = documents.get('passport', 'first_name')
-          document.getElementById('lastName').value = documents.get('passport', 'last_name')
-          document.getElementById('secondName').value = documents.get('passport', 'patronymic')
-          document.getElementById('inn').value = documents.get('INN', 'number')
-        })
-        .catch(err => {
-          err && alert(err)
-        })
+      tim.insertByAttributes(timClient, formElement)
     })
 })
 
