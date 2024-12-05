ed0d733283b3ab6900a0302e2d22d7deef5a3928
Storozhuk Valentine <stolentine@gmail.com>
2024-12-05 21:17:40 +0300

    исправлен пример простого приложения

--- example/simple/index.js
+++ example/simple/index.js
@@ -1,4 +1,6 @@
 window.addEventListener('load', () => {
+  const formElement = document.getElementById('form')
+
   const timClient = new tim.Client({
     id: 0,
     secret: "00000000-0000-0000-0000-000000000000",
